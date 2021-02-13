be40360c1207ff1f7448d0390cd58725d0afa379
Braelyn Rotman <braelynr>
2021-02-13 02:34:44 -0500

    html stuffs

--- app.js
+++ app.js
@@ -2,12 +2,22 @@
 var i = 0;
 var randomNote = "Welcome to Love Letter <3";
 var notes = ['Smile! It\'s a beautiful day!',
-'You\'re an amazing person!'];
+'You\'re an amazing person!',
+'Always remember how much you are loved',
+'Everything is going to be okay. Don\'t let it get to you.',
+'You are perfect in every possible way.',
+'You are enough!',
+'You got this!',
+'You are beautiful!',
+'Have a lovely day!',
+'You are braver than you think.',
+'You are worthy of love',
+'I believe in you.'];
 
 function getNextNote(){
   randomNote = notes[i];
   i++;
-  if (i == 2)
+  if (i == 12)
   {
     i = 0;
   }
@@ -21,5 +31,5 @@ random.addEventListener('click', function() {
 });
 
 // when the page first loads
-window.addEventListener('load', function(){
-});
+//window.addEventListener('load', function(){
+//});
