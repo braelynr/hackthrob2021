f2638eaa18b3054338ef85dd1f3737b39da4bdf0
Braelyn Rotman <braelynr>
2021-02-12 20:50:40 -0500

    update

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
+
 'use strict'
 
 const random = document.querySelector('.btn');
 
 random.addEventListener('click', function() {
   document.getElementById("note").innerHTML = "Test Change";
+});
+
+// when the page first loads
+window.addEventListener('load', function(){
+
 });
