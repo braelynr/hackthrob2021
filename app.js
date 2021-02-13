67433dcb2a519b9fcd1f37bd4e712d7cb8aaaf2b
Braelyn Rotman <braelynr>
2021-02-12 22:01:23 -0500

    putting a pause on db connection to work on front end

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 
-'use strict'
+var i = 0;
+var randomNote = "Welcome to Love Letter <3";
+var notes = ['Smile! It\'s a beautiful day!',
+'You\'re an amazing person!'];
+
+function getNextNote(){
+  randomNote = notes[i];
+  i++;
+  if (i == 2)
+  {
+    i = 0;
+  }
+}
 
 const random = document.querySelector('.btn');
 
 random.addEventListener('click', function() {
-  document.getElementById("note").innerHTML = "Test Change";
+  getNextNote();
+  document.getElementById("note").innerHTML = randomNote;
 });
 
 // when the page first loads
 window.addEventListener('load', function(){
-
 });
